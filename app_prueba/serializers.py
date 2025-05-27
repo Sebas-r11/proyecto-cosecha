e26bd9c975870f3c69c99700754fde961a6917d1
Sebas-r11 <s15601774@gmail.com>
2025-05-26 21:55:56 -0500

    Delete app_prueba directory

--- app_prueba/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from app_prueba.models import usuario 
-class usuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = usuario
-        fields = ['id', 'password', 'rol','nombre', 'email', 'telefono', 'direccion']
